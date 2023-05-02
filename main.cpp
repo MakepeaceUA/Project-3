a9ba25e22fcc11e01c1b5d696206b2b17f11bf18
Arsen Sevastyanov <130586920+Makepease@users.noreply.github.com>
2023-05-02 12:54:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+using namespace std;
+
+//ДЗ№1
+//int main() {
+  
+  //double S;
+  //double V;
+  //setlocale(0, "RUS");
+  
+  //cout << "Введите расстояние:\n";
+  //cin >> S;
+
+  //cout << "Введите скорость:\n";
+  //cin >> V;
+  //cout << "\n";
+
+  //double T = S * 1000 / V;
+  //short Hr = T / 3600;
+  //short Min = (T - Hr * 3600) / 60;
+  //short Sec = T - Hr * 3600 - Min * 60;
+
+  //cout << Hr << " Часа, "  ;
+  //cout << Min << " Минут, ";
+  //cout << Sec << " Секунд ";
+//}
+
+
+
+
+//ДЗ№2
+//int main() {
+//double first_num;
+//double second_num;
+//setlocale(0, "RUS");
+  
+//cout << "Введите первое дробное число:\n";
+//cin >> first_num;
+
+//cout << "Введите второе дробное число:\n";
+//cin >> second_num;
+
+//short first_sum = int(first_num) + int(second_num);  
+//double second_sum = first_num - int(first_num) + second_num - int(second_num);  
+
+//cout << "Сумма целых частей: " << first_sum << "\n";
+//cout << "Сумма дробных частей: " << second_sum << "\n";
+//}
+
+
+
+
+//ДЗ№3
+//int main() {
+//double UAH;
+//setlocale(0, "RUS");
+
+//cout << "Введите количество гривен и копеек:\n";
+//cin >> UAH;
+
+//short first_sum = int(UAH);  
+//double second_sum = UAH - int(UAH); 
+
+//cout << first_sum << " Гривен " << second_sum << " Копеек";
+//}
+
+
+
+
+//ДЗ№5
+//int main() {
+  
+  //int Sec;
+  //setlocale(0, "RUS");
+  
+  //cout << "Введите время в секундах:\n";
+  //cin >> Sec;
+
+  //int Day = Sec / 86400;
+  //Sec %= 86400;
+  //int Hr = Sec / 3600;
+  //Sec %= 3600;
+  //int Min = Sec / 60;
+  //Sec %= 60;
+  
+
+  //cout << Day  << " Дни, "  ;
+  //cout << Hr << " Часы, "  ;
+  //cout << Min << " Минуты, ";
+  //cout << Sec << " Секунды ";
+//}
+
+
+
+
+
+
+
+
+//ДЗ№6
+//int main() {
+  
+//double S;
+//short T;
+//setlocale(0, "RUS");
+  
+//cout << "Введите расстояние до аэропорта(в км):\n";
+//cin >> S;
+
+//cout << "Введите время за которое нужно доехать(в минутах):\n";
+//cin >> T;
+
+//short V = S / T * 60; 
+//cout << V << " КМ/Ч ";
+//}
+
+
+
+
+
+//ДЗ№8
+//int main() {
+//setlocale(0, "RUS");
+//short Sec;
+
+//cout << "Введите время прошедшее с начала рабочего дня(в секундах):\n"; 
+//cin >> Sec;
+
+//short T = 3600 * 8 - Sec;
+//short Hr = T / 3600;
+
+//cout << Hr << " Часов осталось";
+//}
+
+
+
+
+
+
+//ДЗ№9
+//int main() {
+//setlocale(0, "RUS");
+
+//short Prise;
+//short Discount;
+//float Res;
+//short Quantity;
+
+    
+//cout << "Введите стоимость одного ноутбука:\n";
+//cin >> Prise;
+//cout << "Введите количество ноутбуков:\n";
+//cin >> Quantity;
+//cout << "Введите процент скидки:\n";
+//cin >> Discount;
+
+//Res = Prise * Quantity * (100 - Discount) / 100;
+
+//cout << "Общая стоимость заказа: " << Res;
+//}
+
